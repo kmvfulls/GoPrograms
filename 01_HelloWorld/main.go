ef79dbcd847072803e90ada3b84d11f9c1a86905
Kalyan Venkat <kmvmisc@gmail.com>
2018-08-11 20:53:54 -0700

    some new updates

--- 01_HelloWorld/main.go
+++ 01_HelloWorld/main.go
@@ -5,6 +5,15 @@ import "fmt"
 func main() {
 
 	fmt.Println("Hello World !")
+	fmt.Println(42)
+	fmt.Printf("%d- %b", 42, 42)
+	fmt.Println("")
+	// this is a comment
+	/* this is a comment too */
+	/* is this
+		a comment
+		;looks like it
+	 */
 
 }
 
