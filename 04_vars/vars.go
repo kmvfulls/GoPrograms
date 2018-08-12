b19a6b4c0062e2af4a9ffb82fec1f7012602394f
Kalyan Venkat <kmvmisc@gmail.com>
2018-08-11 23:20:17 -0700

    vars project

---
+++ 04_vars/vars.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+var b string
+
+
+func main() {
+
+	a := 10
+	b := "go language"
+	c := 4.18
+	d := true
+
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+
+}
